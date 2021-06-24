9a26be00dc7f8dcf176efb45cf305978017dd021
takuya.ii <takuya.ii@codianz.com>
2021-06-24 17:50:50 +0900

    userController不使用に変更

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,13 +1,13 @@
-import { Post, JsonController, Body } from "routing-controllers";
+import { Post, Body, Controller, Req, Res } from "routing-controllers";
 import * as entity from "../entity";
 import { FindManyOptions, getRepository, Repository } from "typeorm";
 
 let repository: Repository<entity.User | entity.Book>;
 
 function setRepository<T extends entity.entity_name_t>(
-  repo: string
+  entityName: string
 ): Repository<entity.entity_map_t[T]> {
-  return getRepository<entity.entity_map_t[T]>(repo);
+  return getRepository<entity.entity_map_t[T]>(entityName);
 }
 
 type req_base_t = {
@@ -25,30 +25,28 @@ type req_book_t = req_base_t & {
 };
 
 type req_t = req_user_t | req_book_t;
+type res_body_t = Promise<entity.User[]> | Promise<entity.Book[]>;
+type res_t = { length: Promise<number>; body: res_body_t };
 
-// function executeQuery<T extends keyof entity.entity_map_t>(
-//   entity: T,
-//   query: FindManyOptions<entity.entity_map_t[T]>
-// ) {}
-
-@JsonController()
+@Controller()
 export class UserController {
   @Post("/")
-  getAll(@Body() request: req_t) {
-    // if (request.entity == "User") {
-    //   executeQuery(request.entity, request.query);
-    // } else if (request.entity == "Book") {
-    //   executeQuery(request.entity, request.query);
-    // }
+  getAll(@Body() request: req_base_t) {
+    const entity: string = request.entity;
+    repository = setRepository(entity);
+    return repository.find();
+  }
+
+  @Post("/list")
+  getList(@Req() request: req_t, @Res() response: any) {
     const entity: string = request.entity;
     repository = setRepository(entity);
     const query: FindManyOptions = request.query;
-    // if (!query) return repository.find();
-    return repository.find(query);
+    // return repository.find(query);
+    response = {
+      length: repository.count(),
+      body: repository.find(query),
+    };
+    return response;
   }
-
-  // @Post("/list")
-  // postQuery(@Body() query: FindManyOptions) {
-  //   return repository.find(query);
-  // }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,69 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { createExpressServer } from "routing-controllers";
-import { UserController } from "./controller/UserController";
-import { User } from "./entity/User";
-import { Book } from "./entity/Book";
+import * as entity from "./entity";
+import express, { Application } from "express";
+import bodyParser from "body-parser";
+import { FindManyOptions, getRepository, Repository } from "typeorm";
+import cors from "cors";
 
-createConnection({
+const connection = createConnection({
   type: "mysql",
   host: "localhost",
   port: 3333,
   username: "root",
   password: "testpass",
   database: "test",
-  entities: [User, Book],
+  entities: entity.entities,
   synchronize: true,
   logging: "all",
-})
-  .then(async (connection) => {
-    createExpressServer({
-      cors: true,
-      controllers: [UserController],
-    }).listen(3000);
-    console.log("Server is up and running on port 3000.");
-  })
-  .catch((error) => console.log("Error: ", error));
+});
+
+const app = express();
+app.listen(3000);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+let repository: Repository<entity.User | entity.Book>;
+
+type req_base_t = {
+  entity: entity.entity_name_t;
+};
+
+type req_user_t = req_base_t & {
+  entity: "User";
+  query: FindManyOptions<entity.entity_map_t["User"]>;
+};
+
+type req_book_t = req_base_t & {
+  entity: "Book";
+  query: FindManyOptions<entity.entity_map_t["Book"]>;
+};
+
+interface PostRequest extends Request {
+  exparam: req_user_t | req_book_t;
+}
+interface PostResponse extends Response {
+  ResBody: { length: number; body: {} };
+}
+
+function setRepository<T extends entity.entity_name_t>(
+  entityName: string
+): Repository<entity.entity_map_t[T]> {
+  return getRepository<entity.entity_map_t[T]>(entityName);
+}
+
+app.post("/", (req, res) => {
+  const entity: string = req.body.entity;
+  repository = setRepository(entity);
+  const query: FindManyOptions = req.body.query;
+  const _length = repository.count();
+  const _body = repository.find(query);
+  Promise.all([_length, _body])
+    .then(([length, body]) => {
+      res.send({ length, body });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
