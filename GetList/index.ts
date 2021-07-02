6b7ea0b55d914e1eb73c4e49cae1d2a8388e2a82
takuya.ii <takuya.ii@codianz.com>
2021-07-02 20:17:17 +0900

    DELETE処理を実装

--- GetList/index.ts
+++ GetList/index.ts
@@ -2,7 +2,6 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import {
   createConnection,
   FindManyOptions,
-  getConnection,
   getRepository,
   Like,
   Repository,
@@ -37,38 +36,103 @@ function setRepository<T extends entity.EntityName>(
 ): Repository<entity.EntityMap[T]> {
   return getRepository<entity.EntityMap[T]>(entityName);
 }
-
+let res: Promise<void>;
 const httpTrigger: AzureFunction = function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    const request: RequestOptions = req.query!;
-    repository = setRepository(request.entityName!);
-    const query = {
-      where: request.searchColumn
-        ? { [request.searchColumn]: Like(`%${request.searchText}%`) }
-        : {},
-      order: request.orderby ? { [request.orderby]: request.orderdesc } : {},
-      skip: request.skip,
-      take: request.take,
-    };
-    const _length = repository.count(query);
-    const _body = repository.find(query);
-    Promise.all([_length, _body])
-      .then(([length, body]) => {
-        context.res = {
-          body: { length, body },
-        };
-        resolve();
-      })
-      .catch((err) => {
-        context.res = {
-          body: err,
-        };
-        resolve();
-      });
-  });
+  /**
+   * CREATE
+   */
+  if (req.method === "POST") {
+    res = new Promise<void>((resolve, reject) => {});
+  }
+
+  /**
+   * READ
+   */
+  if (req.method === "GET") {
+    res = new Promise<void>((resolve, reject) => {
+      const request: RequestOptions = req.query;
+      repository = setRepository(request.entityName!);
+      const query: FindManyOptions = {
+        where: request.searchColumn
+          ? { [request.searchColumn]: Like(`%${request.searchText}%`) }
+          : {},
+        order: request.orderby ? { [request.orderby]: request.orderdesc } : {},
+        skip: request.skip,
+        take: request.take,
+      };
+      const _length = repository.count(query);
+      const _body = repository.find(query);
+      Promise.all([_length, _body])
+        .then(([length, body]) => {
+          context.res = {
+            body: { length, body },
+          };
+          resolve();
+        })
+        .catch((err) => {
+          context.res = {
+            body: err,
+          };
+          resolve();
+        });
+    });
+  }
+
+  /**
+   * UPDATE
+   */
+  // if (req.method === "PUT") {
+  //   res = new Promise<void>((resolve, reject) => {
+  //     const request = req.query;
+  //     repository = setRepository(request.entityName!);
+  //     const data = repository.findOne(request)
+
+  //     const _update = repository.save(data);
+  //     _update
+  //       .then((value) => {
+  //         context.res = {
+  //           body: value,
+  //         };
+  //         resolve();
+  //       })
+  //       .catch((err) => {
+  //         context.res = {
+  //           body: err,
+  //         };
+  //         resolve();
+  //       });
+  //   });
+  // }
+
+  /**
+   * DELETE
+   */
+  if (req.method === "DELETE") {
+    res = new Promise<void>((resolve, reject) => {
+      const request = req.query;
+      repository = setRepository(request.entityName!);
+      const query = { id: parseInt(request.deleteItem!) };
+      const _delete = repository.delete(query);
+      _delete
+        .then((value) => {
+          context.res = {
+            body: value,
+          };
+          resolve();
+        })
+        .catch((err) => {
+          context.res = {
+            body: err,
+          };
+          resolve();
+        });
+    });
+  }
+
+  return res;
 };
 
 export default httpTrigger;
