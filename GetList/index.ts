2fda820cda7b1e7fb586be5d425f849d9e156905
takuya.ii <takuya.ii@codianz.com>
2021-07-02 13:01:35 +0900

    LIKE検索の実装, SQLqueryを組み立てる部分をサーバー側に移動

--- GetList/index.ts
+++ GetList/index.ts
@@ -2,11 +2,23 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import {
   createConnection,
   FindManyOptions,
+  getConnection,
   getRepository,
+  Like,
   Repository,
 } from "typeorm";
 import * as entity from "../src/entity";
 
+type OrderDesc = "ASC" | "DESC" | undefined;
+type RequestOptions = {
+  entityName?: entity.EntityName;
+  orderby?: string;
+  orderdesc?: OrderDesc;
+  searchColumn?: entity.EntityName;
+  searchText?: string;
+  skip?: number;
+  take?: number;
+};
 const connection = createConnection({
   type: "mysql",
   host: "localhost",
@@ -19,22 +31,29 @@ const connection = createConnection({
   logging: "all",
 });
 
+let repository: Repository<entity.User | entity.Book>;
+function setRepository<T extends entity.EntityName>(
+  entityName: string
+): Repository<entity.EntityMap[T]> {
+  return getRepository<entity.EntityMap[T]>(entityName);
+}
+
 const httpTrigger: AzureFunction = function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
   return new Promise<void>((resolve, reject) => {
-    let repository: Repository<entity.User | entity.Book>;
-    function setRepository<T extends entity.EntityName>(
-      entityName: string
-    ): Repository<entity.EntityMap[T]> {
-      return getRepository<entity.EntityMap[T]>(entityName);
-    }
-
-    const entityName: string = req.query.entity!;
-    const query: FindManyOptions = JSON.parse(req.query.query!);
-    repository = setRepository(entityName);
-    const _length = repository.count();
+    const request: RequestOptions = req.query!;
+    repository = setRepository(request.entityName!);
+    const query = {
+      where: request.searchColumn
+        ? { [request.searchColumn]: Like(`%${request.searchText}%`) }
+        : {},
+      order: request.orderby ? { [request.orderby]: request.orderdesc } : {},
+      skip: request.skip,
+      take: request.take,
+    };
+    const _length = repository.count(query);
     const _body = repository.find(query);
     Promise.all([_length, _body])
       .then(([length, body]) => {
